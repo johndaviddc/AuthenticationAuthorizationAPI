3d578da2d8c1792796240a91e15f76719e793690
John David Dela Cruz <johndaviddc1@gmail.com>
2023-10-23 13:26:52 +0800

    added role repository

---
+++ src/main/java/dave/dev/authapi/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package dave.dev.authapi.repository;
+
+import dave.dev.authapi.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByName(String name);
+}
