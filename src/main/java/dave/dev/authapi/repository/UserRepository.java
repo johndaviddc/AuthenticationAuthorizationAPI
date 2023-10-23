fdf66f51c85333653b8cbbeb2c21a63742feac58
John David Dela Cruz <johndaviddc1@gmail.com>
2023-10-23 13:25:22 +0800

    added user repository

---
+++ src/main/java/dave/dev/authapi/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package dave.dev.authapi.repository;
+
+import dave.dev.authapi.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
