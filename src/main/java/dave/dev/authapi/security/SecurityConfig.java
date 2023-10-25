8ce92edde76cc1728789b337cf58f9ffd7c7c0df
John David Dela Cruz <johndaviddc1@gmail.com>
2023-10-25 13:32:45 +0800

    added security config

--- src/main/java/dave/dev/authapi/security/SecurityConfig.java
+++ src/main/java/dave/dev/authapi/security/SecurityConfig.java
@@ -1,7 +1,10 @@
 package dave.dev.authapi.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -19,6 +22,27 @@ public class SecurityConfig extends WebSecurityConfigurer {
    }
 
    @Override
-    protected void 
+    protected void configure(HttpSecurity http) throws Exception {
+       http
+               .authorizeRequests()
+               .antMatchers("/api/auth/**").permitAll()
+               .antMatchers("/api/roles**").hasRole("ADMIN")
+               .anyRequest().authenticated()
+               .and()
+               .httpBasic();
+
+       http.csrf().disable();
+       http.cors();
+   }
+
+   @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+       auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+   }
+
+   @Bean
+    public JwtTokenFilter jwtTokenFilter() {
+       return new JwtTokenFilter();
+   }
 
 }
