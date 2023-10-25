d0996973f46a75cb35ae73caa8a074bcdcc7f688
John David Dela Cruz <johndaviddc1@gmail.com>
2023-10-25 13:57:36 +0800

    added jwttoken

---
+++ src/main/java/dave/dev/authapi/security/JwtTokenProvider.java
@@ -0,0 +1,42 @@
+package dave.dev.authapi.security;
+
+import lombok.Value;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+
+    @Value("{$security.jwt.token.secret-key}")
+    private String secretKey;
+
+    @Value("${security.jwt.token.expire-length}")
+    private long validityInMilliseconds;
+
+    public String createToken(String username, Collection<? extends GrantedAuthority> authorities) {
+        Claims claims= Jwts.claims().setSubject(username);
+        claims.put("auth", authorities);
+
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + validityInMilliseconds);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(validity)
+                .signWith(SignatureAlgorithm.H256, secretKey)
+                .compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+            return !claims.getBody().getExpiration().before(new Date());
+        } catch (JwtException | IllegalArgumentException e) {
+            throw new JwtAuthenticationException("Expired or invalid JWT token");
+        }
+    }
+}
