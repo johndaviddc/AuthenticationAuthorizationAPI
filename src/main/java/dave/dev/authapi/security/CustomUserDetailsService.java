171041e6f9d41f635ed0c172cbefdeeadb87ca63
John David Dela Cruz <johndaviddc1@gmail.com>
2023-10-25 13:50:15 +0800

    added custom user details

---
+++ src/main/java/dave/dev/authapi/security/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package dave.dev.authapi.security;
+
+import dave.dev.authapi.model.User;
+import dave.dev.authapi.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found with username: " + username);
+        }
+        return user;
+    }
+}
